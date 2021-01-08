55a6ccbf2db816f2b42b2951af51f64e3dae72d8
Russkazkin <ruslan@skazkin.su>
2021-01-08 13:11:04 +0300

    Add app layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from "react";
 
+import Layout from "./components/Layout/Layout";
+
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Font check text</h1>
-      </div>
+      <Layout>
+        <div>Test</div>
+      </Layout>
     );
   }
 }
