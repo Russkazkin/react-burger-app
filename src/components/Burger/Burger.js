92a21e643526fb589f50dc0b029216f5f94448bd
Russkazkin <ruslan@skazkin.su>
2021-01-09 14:50:53 +0300

    Install node-sass and fix sass modules names

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import classes from "./Burger.sass";
+import classes from "./Burger.module.sass";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = (props) => {
