f9af481b7dc27d00f39ea5acba55e2d9d3ccf634
Russkazkin <ruslan@skazkin.su>
2021-01-10 14:11:45 +0300

    Add some styling tailwind classes

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,10 +3,10 @@ import React from "react";
 import classes from "./BuildControl.module.sass"
 
 const buildControl = ({label}) => (
-  <div className={"flex justify-between items-center my-1.5 " + classes.BuildControl}>
-    <div>{label}</div>
-    <button className="block p-1.5 mx-1.5 w-20 border cursor-pointer outline-none">Less</button>
-    <button className="block p-1.5 mx-1.5 w-20 border cursor-pointer outline-none">More</button>
+  <div className={"flex justify-center items-center my-1.5 container mx-auto " + classes.BuildControl}>
+    <div className="w-1/4 text-left font-bold text-white">{label}</div>
+    <button className="block p-1.5 mx-1.5 w-20 text-white border cursor-pointer outline-none bg-indigo-500 rounded-full hover:bg-indigo-300 hover:text-gray-700">Less</button>
+    <button className="block p-1.5 mx-1.5 w-20 text-white border cursor-pointer outline-none bg-red-500 rounded-full hover:bg-red-300 hover:text-gray-700">More</button>
   </div>
 );
 
