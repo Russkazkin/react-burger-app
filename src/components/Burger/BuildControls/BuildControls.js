3cc1767a2331b838166b1be19c33160167846687
Russkazkin <ruslan@skazkin.su>
2021-01-10 13:59:10 +0300

    Output BuildControls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,19 @@
 import React from "react";
 import classes from "./BuildControls.module.sass";
+import BuildControl from "./BuildControl/BuildControl"
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
-  <div className="w-full flex flex-col text-center shadow-sm m-auto py-2.5">
-
+  <div className={"w-full flex flex-col text-center shadow-sm m-auto py-2.5 " + classes.BuildControls}>
+    {controls.map((ctrl) => (
+      <BuildControl key={ctrl.label} label={ctrl.label}/>
+    ))}
   </div>
 );
 
