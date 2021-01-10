3564514bbfbd0fbab3fb40cdccbf3de5ff8f351b
Russkazkin <ruslan@skazkin.su>
2021-01-10 13:47:59 +0300

    Add BuildControls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./BuildControls.module.sass";
+
+const buildControls = (props) => (
+  <div className="w-full flex flex-col text-center shadow-sm m-auto py-2.5">
+
+  </div>
+);
+
+export default buildControls;
