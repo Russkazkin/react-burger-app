9f837dfcf3a8094587d89e736b8eca80cd833d02
Russkazkin <ruslan@skazkin.su>
2021-01-10 11:47:10 +0300

    Flatten ingredients array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from "./Burger.module.sass";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = ({ingredients}) => {
-  const transformedIngredients = Object.keys(ingredients).map(ingredientKey => {
+  let transformedIngredients = Object.keys(ingredients).map(ingredientKey => {
     return [...Array(ingredients[ingredientKey])].map((_, index) => {
       return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
     });
-  });
+  }).reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return (
     <div className={'m-auto overflow-scroll text-center font-bold text-lg ' + classes.Burger}>
       <BurgerIngredient type="bread-top" />
