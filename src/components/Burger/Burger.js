c52afdb91e646d73505cf293481a3a29639e4e05
Russkazkin <ruslan@skazkin.su>
2021-01-10 11:21:32 +0300

    Add ingredients output from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import React from "react";
 import classes from "./Burger.module.sass";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
-const burger = (props) => {
+const burger = ({ingredients}) => {
+  const transformedIngredients = Object.keys(ingredients).map(ingredientKey => {
+    return [...Array(ingredients[ingredientKey])].map((_, index) => {
+      return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+    });
+  });
   return (
     <div className={'m-auto overflow-scroll text-center font-bold text-lg ' + classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
