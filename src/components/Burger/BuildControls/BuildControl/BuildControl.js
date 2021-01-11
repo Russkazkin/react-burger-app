8fe1a940e5094eb1a47e0d2af1584c4b8c989a0a
Russkazkin <ruslan@skazkin.su>
2021-01-11 17:52:16 +0300

    Fix buttons outline

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,10 +5,10 @@ import classes from "./BuildControl.module.sass"
 const buildControl = ({label, added}) => (
   <div className={"flex justify-center items-center my-1.5 container mx-auto " + classes.BuildControl}>
     <div className="w-1/4 text-left font-bold text-white">{label}</div>
-    <button className="block p-1.5 mx-1.5 w-20 text-white border cursor-pointer outline-none bg-indigo-500 rounded-full hover:bg-indigo-300 hover:text-gray-700"
+    <button className="block p-1.5 mx-1.5 w-20 text-white border cursor-pointer focus:outline-none focus:outline-white bg-indigo-500 rounded-full hover:bg-indigo-300 hover:text-gray-700"
             >Less
     </button>
-    <button className="block p-1.5 mx-1.5 w-20 text-white border cursor-pointer outline-none bg-red-500 rounded-full hover:bg-red-300 hover:text-gray-700"
+    <button className="block p-1.5 mx-1.5 w-20 text-white border cursor-pointer focus:outline-none focus:outline-white bg-red-500 rounded-full hover:bg-red-300 hover:text-gray-700"
             onClick={added}>More
     </button>
   </div>
