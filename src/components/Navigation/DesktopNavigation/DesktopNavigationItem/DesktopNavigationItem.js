9217ae7b6e869e3caaff5430ae6688ae70d7d31c
Russkazkin <ruslan@skazkin.su>
2021-01-18 18:04:18 +0300

    Add custom brown color to tailwind config

--- src/components/Navigation/DesktopNavigation/DesktopNavigationItem/DesktopNavigationItem.js
+++ src/components/Navigation/DesktopNavigation/DesktopNavigationItem/DesktopNavigationItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const desktopNavigationItem = ({isActive, children}) => (
-  <button className={`${isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} px-3 py-2 rounded-md text-sm font-medium">Dashboard`}>
+  <button className={`${isActive ? 'bg-brown-darkest text-white' : 'text-gray-100 hover:bg-brown-dark hover:text-white'} px-3 py-2 rounded-md text-sm font-medium">Dashboard`}>
     {children}
   </button>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          lightest: '#cf8f2e',
+          light: '#e27b36',
+          DEFAULT: '#bc581e',
+          dark: '#7f3608',
+          darkest: '#702e05',
+        },
+      },
+    },
   },
   variants: {
     extend: {
