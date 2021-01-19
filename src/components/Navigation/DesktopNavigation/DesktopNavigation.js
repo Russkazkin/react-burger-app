c5168d93ab3a8ae254d0b16a1041529c68793161
Russkazkin <ruslan@skazkin.su>
2021-01-19 17:21:41 +0300

    Add minor fixes

--- src/components/Navigation/DesktopNavigation/DesktopNavigation.js
+++ src/components/Navigation/DesktopNavigation/DesktopNavigation.js
@@ -5,10 +5,8 @@ import DesktopNavigationItem from './DesktopNavigationItem/DesktopNavigationItem
 const desktopNavigation = (props) => (
   <div className="hidden sm:block sm:ml-6">
     <div className="flex space-x-4">
-      <DesktopNavigationItem isActive={true}>Dashboard</DesktopNavigationItem>
-      <DesktopNavigationItem isActive={false}>Team</DesktopNavigationItem>
-      <DesktopNavigationItem isActive={false}>Projects</DesktopNavigationItem>
-      <DesktopNavigationItem isActive={false}>Calendar</DesktopNavigationItem>
+      <DesktopNavigationItem link='/' active>Burger Builder</DesktopNavigationItem>
+      <DesktopNavigationItem link='/'>Checkout</DesktopNavigationItem>
     </div>
   </div>
 );
--- src/components/Navigation/DesktopNavigation/DesktopNavigationItem/DesktopNavigationItem.js
+++ src/components/Navigation/DesktopNavigation/DesktopNavigationItem/DesktopNavigationItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const desktopNavigationItem = ({isActive, children}) => (
-  <button className={`${isActive ? 'bg-brown-darkest text-white' : 'text-gray-100 hover:bg-brown-dark hover:text-white'} px-3 py-2 rounded-md text-sm font-medium">Dashboard`}>
+const desktopNavigationItem = ({active, children, link}) => (
+  <a href={link} className={`${active ? 'bg-brown-darkest text-white' : 'text-gray-100 hover:bg-brown-dark hover:text-white'} px-3 py-2 rounded-md text-sm font-medium">Dashboard`}>
     {children}
-  </button>
+  </a>
 );
 
 export default desktopNavigationItem;
