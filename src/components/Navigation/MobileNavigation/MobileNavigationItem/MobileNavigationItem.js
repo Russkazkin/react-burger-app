369f558e1aafc4a20dd1b65e26b0580f95a3d8db
Russkazkin <ruslan@skazkin.su>
2021-01-20 17:40:45 +0300

    Adjust mobile nav styles

--- src/components/Navigation/MobileNavigation/MobileNavigationItem/MobileNavigationItem.js
+++ src/components/Navigation/MobileNavigation/MobileNavigationItem/MobileNavigationItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const  mobileNavigationItem = ({link, children, active}) => (
   <a href={link}
-     className={`${active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} block px-3 py-2 rounded-md text-base font-medium`}>
+     className={`${active ? 'bg-brown-darkest text-white' : 'text-gray-100 hover:bg-brown-dark hover:text-white'} block px-3 py-2 rounded-md text-base font-medium`}>
     {children}
   </a>
 );
