2c52cbff0a411ffc5839b8d254cf62ee00aca4e3
Russkazkin <ruslan@skazkin.su>
2021-02-06 10:40:02 +0300

    Move interceptors into render method

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,16 +6,6 @@ const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
     state = {error: null};
 
-    componentWillMount() {
-      this.requestInterceptor = axios.interceptors.request.use(request => {
-        this.setState({error: null});
-        return request;
-      });
-      this.responseInterceptor = axios.interceptors.response.use(response => response, error => {
-        this.setState({error: error});
-      });
-    }
-
     componentWillUnmount() {
       axios.interceptors.request.eject(this.requestInterceptor);
       axios.interceptors.response.eject(this.responseInterceptor);
@@ -26,6 +16,13 @@ const withErrorHandler = (WrappedComponent, axios) => {
     }
 
     render() {
+      this.requestInterceptor = axios.interceptors.request.use(request => {
+        this.setState({error: null});
+        return request;
+      });
+      this.responseInterceptor = axios.interceptors.response.use(response => response, error => {
+        this.setState({error: error});
+      });
       return (
         <>
           <Modal show={this.state.error} closeModal={this.errorConfirmedHandler}>
