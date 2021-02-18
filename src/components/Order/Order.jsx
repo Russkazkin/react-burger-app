90668567e3d959fd2a18416162f1e59c8b63a1a2
Russkazkin <ruslan@skazkin.su>
2021-02-18 17:41:06 +0300

    Add dummy orders page

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const order = (props) => (
+  <div className="w-full border border-2 border-gray-500 shadow-md p-5 my-5 mx-auto">
+    <p>Ingredients: Salad (1)</p>
+    <p>Price: <strong>USD 5.45</strong></p>
+  </div>
+);
+
+export default order;
