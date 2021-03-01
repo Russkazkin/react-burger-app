268e283164282e87e42f695bbe59e3d45fb5abdb
Russkazkin <ruslan@skazkin.su>
2021-03-01 17:30:34 +0300

    Install and setup redux

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import dotenv from "dotenv";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
 
 import "typeface-open-sans";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer from "./store/reducer";
 
 dotenv.config({path: '../.env'});
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
