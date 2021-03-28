a5e8487ff2f0f6098362016176b3fab9924d1893
Russkazkin <ruslan@skazkin.su>
2021-03-28 10:05:22 +0300

    Turn off redux redux devtools for production

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import authReducer from "./store/reducers/auth";
 
 dotenv.config({path: '../.env'});
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 const rootReducer = combineReducers({
   burgerBuilderReducer,
