f4e3b11f546378cdcfcb91e046c199d60431fe39
Russkazkin <ruslan@skazkin.su>
2021-03-31 19:39:16 +0300

    Fix DesktopNavigation test

--- src/components/Navigation/DesktopNavigation/DesktopNavigation.test.js
+++ src/components/Navigation/DesktopNavigation/DesktopNavigation.test.js
@@ -2,13 +2,14 @@ import React from "react";
 import {configure, shallow} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import DesktopNavigation from "./DesktopNavigation";
-import DesktopNavigationItem from './DesktopNavigationItem/DesktopNavigationItem'
+import DesktopNavigationItem from './DesktopNavigationItem/DesktopNavigationItem';
 
 configure({ adapter: new Adapter() });
 
 describe('<DesktopNavigation />', () => {
   it('should render two <DesktopNavigationItem /> elements if not authenticated', () => {
-    const wrapper = shallow(<DesktopNavigation />);
+    const history = {location: {path: '/fake-path'}};
+    const wrapper = shallow(<DesktopNavigation.WrappedComponent history={history} />);
     expect(wrapper.find(DesktopNavigationItem)).toHaveLength(2);
   });
 });
