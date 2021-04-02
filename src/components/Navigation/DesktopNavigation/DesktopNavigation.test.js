d468c884c4023a04ccd1f1903b6adeade4c5e4f8
Russkazkin <ruslan@skazkin.su>
2021-04-02 17:19:37 +0300

    Add fixture for tests

--- src/components/Navigation/DesktopNavigation/DesktopNavigation.test.js
+++ src/components/Navigation/DesktopNavigation/DesktopNavigation.test.js
@@ -7,14 +7,17 @@ import DesktopNavigationItem from './DesktopNavigationItem/DesktopNavigationItem
 configure({ adapter: new Adapter() });
 
 describe('<DesktopNavigation />', () => {
+  let history, wrapper;
+  beforeEach(() => {
+    history = {location: {path: '/fake-path'}};
+    wrapper = shallow(<DesktopNavigation.WrappedComponent history={history} />);
+  });
+
   it('should render two <DesktopNavigationItem /> elements if not authenticated', () => {
-    const history = {location: {path: '/fake-path'}};
-    const wrapper = shallow(<DesktopNavigation.WrappedComponent history={history} />);
     expect(wrapper.find(DesktopNavigationItem)).toHaveLength(2);
   });
   it('should render three <DesktopNavigationItem /> elements if authenticated', () => {
-    const history = {location: {path: '/fake-path'}};
-    const wrapper = shallow(<DesktopNavigation.WrappedComponent history={history} isAuthenticated={true} />);
+    wrapper.setProps({isAuthenticated: true});
     expect(wrapper.find(DesktopNavigationItem)).toHaveLength(3);
   });
 });
