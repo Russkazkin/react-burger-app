9143007683b7c5d44ce00f61b474c3283f8ab47e
Russkazkin <ruslan@skazkin.su>
2021-04-02 17:31:31 +0300

    Add test for active navigation item

--- src/components/Navigation/DesktopNavigation/DesktopNavigation.test.js
+++ src/components/Navigation/DesktopNavigation/DesktopNavigation.test.js
@@ -4,13 +4,13 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import DesktopNavigation from "./DesktopNavigation";
 import DesktopNavigationItem from './DesktopNavigationItem/DesktopNavigationItem';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 describe('<DesktopNavigation />', () => {
   let history, wrapper;
   beforeEach(() => {
-    history = {location: {path: '/fake-path'}};
-    wrapper = shallow(<DesktopNavigation.WrappedComponent history={history} />);
+    history = {location: {pathname: '/logout'}};
+    wrapper = shallow(<DesktopNavigation.WrappedComponent history={history}/>);
   });
 
   it('should render two <DesktopNavigationItem /> elements if not authenticated', () => {
@@ -20,4 +20,9 @@ describe('<DesktopNavigation />', () => {
     wrapper.setProps({isAuthenticated: true});
     expect(wrapper.find(DesktopNavigationItem)).toHaveLength(3);
   });
-});
+  it('should render <DesktopNavigationItem /> with logout link', () => {
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.contains(<DesktopNavigationItem link="/logout" active={true}>Logout</DesktopNavigationItem>)).toEqual(true);
+  });
+}
+);
